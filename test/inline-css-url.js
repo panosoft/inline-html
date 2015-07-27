539da292da77902fd554af2ad6e16619c61bde8e
Alexandre Gigliotti <alex@panosoft.com>
2015-07-27 13:15:45 -0700

    Switched to postcss.

--- test/inline-css-url.js
+++ test/inline-css-url.js
@@ -3,5 +3,5 @@ var fs = require('fs');
 
 var filePath = './fixtures/assets/imported.css';
 var css = fs.readFileSync(filePath, 'utf8');
-css = inline(css, filePath);
-console.log(css);
+var result = inline(css, filePath);
+console.log(result.css);
